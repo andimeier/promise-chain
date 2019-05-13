e555f2118572d53c75941d3bbb019d4b14ff8c88
Alexander Eck-Zimmer <alexander.eck-zimmer@siemens.com>
2019-05-13 18:43:06 +0200

    nesting with arrays works too

--- test/test.js
+++ test/test.js
@@ -5,16 +5,58 @@ const ok = () => Promise.resolve();
 const nok = () => Promise.reject("oh no");
 
 promiseChain
-  .execute({
-    name: "A",
-    promise: ok,
-    onResolve: {
-      name: "B",
+  .execute([
+    {
+      name: "A",
       promise: nok,
-      onReject: "#2"
+      onReject: "#1"
     },
-    onReject: "#1"
+    {
+      name: "B",
+      promise: ok,
+      onResolve: {
+        name: "C",
+        promise: nok,
+        onReject: "#3"
+      },
+      onReject: "#2"
+    }
+  ])
+  .then(() => {
+    console.log("everything ok");
   })
+  .catch(errors => {
+    console.log("There have been errors:");
+    errors.forEach(error => {
+      console.log(`${error.code}: ${error.msg}`);
+    });
+  });
+
+return;
+
+promiseChain
+  .execute([
+    {
+      name: "A",
+      promise: ok,
+      onResolve: {
+        name: "B",
+        promise: ok,
+        onReject: "#2"
+      },
+      onReject: "#1"
+    },
+    {
+      name: "C",
+      promise: ok,
+      onResolve: {
+        name: "D",
+        promise: ok,
+        onReject: "#4"
+      },
+      onReject: "#3"
+    }
+  ])
   .then(() => {
     console.log("everything ok");
   })
